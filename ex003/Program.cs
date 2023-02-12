9328c8b7bf8eb316a219f18967088a6cd3ab34e0
DLitvinenko <dimaboblitvinenko@gmail.com>
2023-02-12 14:35:55 +0300

    ex04

---
+++ ex003/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая будет принимать на вход 
+//два числа и выводить, является ли второе число кратным 
+//первому. Если число 2 не кратно числу 1, то программа 
+//выводит остаток от деления.
+
+
+Console.Clear();
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if(n2 % n1 == 0)
+{
+  Console.WriteLine("Число кратно");
+}
+else
+{
+  Console.WriteLine($"No, {n2%n1}");
+}
---
+++ ex004/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число 
+//и проверяет, кратно ли оно одновременно 7 и 23.
+
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+if((n % 7 == 0) && (n % 23 == 0))
+  Console.WriteLine("Число кратно");
+else
+  Console.WriteLine("Число не кратно");
