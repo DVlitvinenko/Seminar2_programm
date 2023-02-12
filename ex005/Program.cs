3208f6920fb6e4b9fa9aa1819500fcaa9a2f5a27
DLitvinenko <dimaboblitvinenko@gmail.com>
2023-02-12 14:50:11 +0300

    over

---
+++ ex005/Program.cs
@@ -0,0 +1,13 @@
+﻿// 16. Напишите программу, которая принимает на вход два 
+//числа и проверяет,
+// является ли одно число квадратом другого.
+
+
+Console.Clear();
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if(n2 == n1*n1 || n1 == n2*n2)
+  Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
